262954838b3f070269e96fe9ac74d87b665a23a0
Smos Bois <simondeb1505@gmail.com>
2024-01-27 00:00:24 +0100

    Environment done

--- Assets/Scripts/PuppetMaster.cs
+++ Assets/Scripts/PuppetMaster.cs
@@ -4,49 +4,20 @@ using UnityEngine;
 
 public class PuppetMaster : MonoBehaviour
 {
+    public Transform rightEye, leftEye;
+    public Transform rightLookAt, leftLookAt;
 
-    [Header("Mouth")]
-    public Transform fullMouth;
-    public Transform LowerJamJoint;
-    public Vector3 RaisedMouthPos, LoweredMouthPos;
-    public float raiseSpeed, lowerSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        RaisedMouthPos = transform.position + RaisedMouthPos;
-        LoweredMouthPos = transform.position + LoweredMouthPos;
-
-        LowerTheMouth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void LowerTheMouth()
-    {
-        StartCoroutine(LowerMouth());
-    }
-
-    public IEnumerator LowerMouth()
-    {
-        bool done = false;
-
-        while (!done)
-        {
-            fullMouth.transform.position += (LoweredMouthPos - fullMouth.transform.position).normalized * Time.deltaTime * lowerSpeed;
-
-            if (Vector3.Distance(fullMouth.transform.position, LoweredMouthPos) < 0.1f)
-            {
-                done = true;
-            }
-
-            yield return new WaitForEndOfFrame();
-
-        }
+        rightEye.transform.LookAt(rightLookAt);
+        leftEye.transform.LookAt(leftLookAt);
     }
 }
