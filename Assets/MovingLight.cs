aa5104dddc56ad918e2d3848c1ecb23dcdbe27b2
Smos Bois <simondeb1505@gmail.com>
2024-01-26 17:20:26 +0100

    Succesfully added pixelizer pipeline

---
+++ Assets/MovingLight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLight : MonoBehaviour
+{
+    public Vector3 pointA, pointB;
+    public float movespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move from A to B and vice versa
+    }
+}
